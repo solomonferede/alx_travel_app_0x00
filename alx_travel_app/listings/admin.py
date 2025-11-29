4be8b5edb0f84e952d94f59a9908b0487c02f4b6
solomonferede1 <solomonf1227@gmail.com>
2025-11-29 19:32:16 +0300

    Register models in the admin site

--- alx_travel_app/listings/admin.py
+++ alx_travel_app/listings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Listing, Booking, Review
 
-# Register your models here.
+# Simple registration
+admin.site.register(Listing)
+admin.site.register(Booking)
+admin.site.register(Review)
